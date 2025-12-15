792c78ae6bf240202950ad380436a4ce919c59e4
veronika219 <Dmytrenko.V.S@nmu.one>
2025-12-16 00:18:17 +0200

    Updated tests for function - get_human_age with pytest parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,22 +1,18 @@
+import pytest
 from app.main import get_human_age
 
-def test_check_if_cat_age_is_equal_0() -> None:
-    assert get_human_age(0, 98) == [0, 3]
 
-def test_if_animals_age_less_15_equal_empty_list() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-
-def test_check_if_both_age_14_boundary() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-def test_check_if_dog_age_is_equal_0() -> None:
-    assert get_human_age(98, 0) == [3, 0]
-
-def test_check_if_both_age_is_15() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-def test_check_if_both_age_is_24() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-def test_check_if_max_age_for_both() -> None:
-    assert get_human_age(28,29) == [3, 3]
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected",
+    [
+        pytest.param(0, 0, [0,0], id="check empty list"),
+        pytest.param(0, 98, [0, 3], id ="check if only cat age is equal 0"),
+        pytest.param(98, 0, [3, 0], id="check if only dog age is equal 0"),
+        pytest.param(14, 14, [0, 0], id="check if both age 14 boundary"),
+        pytest.param(15, 15, [1, 1], id="check if both age is 15"),
+        pytest.param(24, 24, [2, 2], id="check if both age is 24"),
+        pytest.param(28, 29, [3, 3], id="check unic-max age for both")
+    ]
+)
+def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
